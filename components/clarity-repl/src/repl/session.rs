d2f06339bf1b432ff8fe4d09cf56d2e2d150ff91
Jeff Bencin <jeff.bencin@gmail.com>
2025-06-24 16:49:59 -0400

    chore: Apply `clippy::string_lit_as_bytes`

--- components/clarity-repl/src/repl/session.rs
+++ components/clarity-repl/src/repl/session.rs
@@ -1335,8 +1335,7 @@ mod tests {
         };
         assert_eq!(
             res.result,
-            Value::some(Value::string_ascii_from_bytes("stack".as_bytes().to_vec()).unwrap())
-                .unwrap()
+            Value::some(Value::string_ascii_from_bytes(b"stack".to_vec()).unwrap()).unwrap()
         );
     }
 
